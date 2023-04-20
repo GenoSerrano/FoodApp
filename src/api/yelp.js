d42ff5c6dfaef18af94a42acb5cd5c0827a1a599
Geno Serrano <genoalfredarmayanserrano@gmail.com>
2023-04-20 19:38:59 +0800

    commit comments

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { APIKEY_YELP } from '@env';
 
+//Environment secret keys will not be pushed to public github
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
